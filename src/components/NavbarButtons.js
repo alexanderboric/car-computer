f34d17900b5a47490c97dbb4b64ee97eda21f03f
FloatDev <54747366+TheRealFloatDev@users.noreply.github.com>
2022-06-07 11:56:46 +0200

    Code cleanup

--- src/components/NavbarButtons.js
+++ src/components/NavbarButtons.js
@@ -6,15 +6,11 @@ import './Navbar.css';
 
 
 export default class NavbarButton extends Component{
-    constructor(props) {
-        super(props);
-        
-      }
-
       render() {
         
         
         return(
+          
         <div className="Navbar-Buttons" onClick={this.props.onClick}>
             <img src={this.props.src} className="Navbar-Button-Images" style={{filter: "invert(1)"}} alt="Navbar Button"></img>
           
