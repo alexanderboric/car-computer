e6b5beef10a0e4cc167aec511922a1cd77cee146
FloatDev <54747366+TheRealFloatDev@users.noreply.github.com>
2022-06-07 11:53:26 +0200

    Code cleanup in App

--- src/App.js
+++ src/App.js
@@ -1,12 +1,6 @@
 import './App.css';
-import ReactDOM from "react-dom";
-import React, { useRef, useState,Suspense, Component, } from 'react';
+import React from 'react';
 
-import Home from './components/Home'
-import Music from './components/Music'
-import Navbar from './components/Navbar';
-import Settings from './components/Settings'
-import Radio from './components/Radio'
 import Manager from './components/Manager';
 
 export default function App() {
