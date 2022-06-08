cb9cc84eab1c0727166ef9e586a2b42bcba9866f
FloatDev <54747366+TheRealFloatDev@users.noreply.github.com>
2022-06-08 22:52:42 +0200

    Added MantineProvider

--- src/App.js
+++ src/App.js
@@ -2,10 +2,13 @@ import './App.css';
 import React from 'react';
 
 import Manager from './components/Manager';
+import { MantineProvider } from '@mantine/core';
 
 export default function App() {
   return (
-    <Manager></Manager>
+    <MantineProvider theme={{ fontFamily: 'Open Sans' }} withGlobalStyles>
+      <Manager />
+    </MantineProvider>
   );
 }
 
