49b9ce183f752a0fbc3d54876c635b2a8f13d6bc
FloatDev <54747366+TheRealFloatDev@users.noreply.github.com>
2022-06-08 23:11:44 +0200

    Improved Navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,14 +4,11 @@ import './Navbar.css'
 
 import icons from '../assets/svg-icons/index';
 import NavbarButton from './NavbarButtons';
-import { useNavigate } from 'react-router-dom';
 import { Box } from '@mantine/core';
 
 
 
 export default function Navbar(props) {
-
-  let navigate = useNavigate();
 
     return(
 
