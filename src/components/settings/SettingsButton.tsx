30182b2a973ab09a46b5e993dcae574cebef9996
FloatDev <54747366+TheRealFloatDev@users.noreply.github.com>
2022-06-09 12:50:56 +0200

    Added new Button

--- src/components/settings/SettingsButton.tsx
+++ src/components/settings/SettingsButton.tsx
@@ -1,21 +1,25 @@
-import { Group, UnstyledButton, Text } from "@mantine/core";
+import { Group, UnstyledButton, Text, Button } from "@mantine/core";
 import * as React from "react";
 import { MdChevronRight } from "react-icons/md";
 import { Link } from "react-router-dom";
 
-export default function SettingsButton({ pageLink, label }: { pageLink: string, label: string }) {
+export default function SettingsButton({
+	pageLink,
+	label,
+    icon
+}: {
+	pageLink: string;
+	label: string;
+    icon?: any;
+}) {
 	return (
 		<>
-			<UnstyledButton
-				sx={(theme) => ({
-					fontWeight: 500,
-				})}
-                
-				component={Link}
-				to={"/settings/" + pageLink}
-			>
-				<Group position="apart">
-					<Text size={"xl"}>{label}</Text>
+			<UnstyledButton component={Link} to={"/settings/"}>
+				<Group position="apart" noWrap mb={3} mt={3}>
+					<Group>
+                        {icon}
+						<Text size="xl">{label}</Text>
+					</Group>
 					<MdChevronRight />
 				</Group>
 			</UnstyledButton>
