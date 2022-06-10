c2923524636d72027849cab709be247f22089a6e
FloatDev <54747366+TheRealFloatDev@users.noreply.github.com>
2022-06-10 10:28:35 +0200

    updated osk

--- src/components/OnScreenKeyboard.tsx
+++ src/components/OnScreenKeyboard.tsx
@@ -1,14 +1,9 @@
 import * as React from "react";
-import Keyboard from "react-simple-keyboard";
-import "react-simple-keyboard/build/css/index.css";
 
 export default function OnScreenKeyboard({ onChange }: { onChange: (value: string) => void }) {
 
     return (
         <>
-            <Keyboard 
-            onChange={onChange}
-            />
         </>
     )
 }
