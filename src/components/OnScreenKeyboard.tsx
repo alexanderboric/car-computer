2a572eaf7ddb62d072846e22bc0c4974a8f54454
FloatDev <54747366+TheRealFloatDev@users.noreply.github.com>
2022-06-10 11:04:37 +0200

    Updated OSK

--- src/components/OnScreenKeyboard.tsx
+++ src/components/OnScreenKeyboard.tsx
@@ -1,6 +1,7 @@
 import { Button, Group, Stack } from "@mantine/core";
 import * as React from "react";
-import { MdBackspace } from "react-icons/md";
+import { MdBackspace, MdOutlineEmojiEmotions, MdSubdirectoryArrowLeft } from "react-icons/md";
+import { BsShift, BsShiftFill } from "react-icons/bs";
 
 export default function OnScreenKeyboard({
 	text,
@@ -27,6 +28,7 @@ export default function OnScreenKeyboard({
 	) => {
 		return (
 			<Button
+                size="sm"
 				variant="default"
 				onClick={() => {
 					if (key) {
@@ -36,29 +38,79 @@ export default function OnScreenKeyboard({
 					}
 				}}
 			>
-				{key ? element : shift ? upperCase : lowerCase}
+				{element ? element : shift ? upperCase : lowerCase}
 			</Button>
 		);
 	};
 
-	const keyRow = (<>
-        <Group>
-            {keyButton("1", "1")}
-            {keyButton("2", "2")}
-            {keyButton("3", "3")}
-            {keyButton("4", "4")}
-            {keyButton("5", "5")}
-            {keyButton("6", "6")}
-            {keyButton("7", "7")}
-            {keyButton("8", "8")}
-            {keyButton("9", "9")}
-            {keyButton("0", "0")}
+	const keyRow = (
+		<>
+			<Group position="apart">
+				{keyButton("1", "1")}
+				{keyButton("2", "2")}
+				{keyButton("3", "3")}
+				{keyButton("4", "4")}
+				{keyButton("5", "5")}
+				{keyButton("6", "6")}
+				{keyButton("7", "7")}
+				{keyButton("8", "8")}
+				{keyButton("9", "9")}
+				{keyButton("0", "0")}
 
-            {keyButton(undefined, undefined, "backspace", <MdBackspace />)}
-        </Group>
-    </>);
-	const symbolRow = (<>
-    </>);
+				{keyButton(undefined, undefined, "backspace", <MdBackspace />)}
+			</Group>
+			<Group position="apart">
+				{keyButton("q", "Q")}
+				{keyButton("w", "W")}
+				{keyButton("e", "E")}
+				{keyButton("r", "R")}
+				{keyButton("t", "T")}
+				{keyButton("z", "Z")}
+				{keyButton("u", "U")}
+				{keyButton("i", "I")}
+				{keyButton("o", "O")}
+				{keyButton("p", "P")}
+				{keyButton("ü", "Ü")}
+			</Group>
+			<Group position="apart">
+				{keyButton("a", "A")}
+				{keyButton("s", "S")}
+				{keyButton("d", "D")}
+				{keyButton("f", "F")}
+				{keyButton("g", "G")}
+				{keyButton("h", "H")}
+				{keyButton("j", "J")}
+				{keyButton("k", "K")}
+				{keyButton("l", "L")}
+				{keyButton("ö", "Ö")}
+				{keyButton("ä", "Ä")}
+			</Group>
+			<Group position="apart">
+				{keyButton(
+					undefined,
+					undefined,
+					"shift",
+					shift ? <BsShiftFill /> : <BsShift />
+				)}
+				{keyButton("y", "Y")}
+				{keyButton("x", "X")}
+				{keyButton("c", "C")}
+				{keyButton("v", "V")}
+				{keyButton("b", "B")}
+				{keyButton("n", "N")}
+				{keyButton("m", "M")}
+				{keyButton(",", ";")}
+				{keyButton(".", ":")}
+				{keyButton("-", "_")}
+			</Group>
+			<Group position="apart">
+                {keyButton(undefined, undefined, "symbol", <MdOutlineEmojiEmotions />)}
+                {keyButton("SPACE", "SPACE", "space", undefined)}
+                {keyButton(undefined, undefined, "enter", <MdSubdirectoryArrowLeft />)}
+            </Group>
+		</>
+	);
+	const symbolRow = <></>;
 
 	return (
 		<>
