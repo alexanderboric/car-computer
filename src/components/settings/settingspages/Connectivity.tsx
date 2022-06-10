2818a0b0b4e78e6170ef7373f9883c0e2e9ae803
FloatDev <54747366+TheRealFloatDev@users.noreply.github.com>
2022-06-10 09:57:09 +0200

    Updated Connectivity Page

--- src/components/settings/settingspages/Connectivity.tsx
+++ src/components/settings/settingspages/Connectivity.tsx
@@ -1,6 +1,8 @@
+import { Divider } from "@mantine/core";
 import * as React from "react";
 import SettingsContainer from "../SettingsContainer";
 import SettingsPage from "../SettingsPage";
+import SettingsStatus from "../SettingsStatus";
 import SettingsSwitch from "../SettingsSwitch";
 
 export default function ConnectivityPage() {
@@ -9,6 +11,8 @@ export default function ConnectivityPage() {
 			<SettingsPage title={"Connectivity"}>
 				<SettingsContainer label="OpenDrop" bottomText="OpenDrop is an open AirDrop implementation. Please note that this feature is experimental and might have to be restarted to work correctly.">
                     <SettingsSwitch label={"Enable AirDrop"} onSwitch={() => {}} />
+                    <Divider />
+                    <SettingsStatus label={"AirDrop Status"} status={"Disabled"}/>
                 </SettingsContainer>
 			</SettingsPage>
 		</>
