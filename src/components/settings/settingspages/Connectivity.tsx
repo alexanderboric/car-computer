1884cfb1b3fc5ebfbd76ac0f20643e82c3242633
FloatDev <54747366+TheRealFloatDev@users.noreply.github.com>
2022-06-10 09:52:30 +0200

    Updated Connectivity Settings

--- src/components/settings/settingspages/Connectivity.tsx
+++ src/components/settings/settingspages/Connectivity.tsx
@@ -1,13 +1,14 @@
 import * as React from "react";
 import SettingsContainer from "../SettingsContainer";
 import SettingsPage from "../SettingsPage";
+import SettingsSwitch from "../SettingsSwitch";
 
 export default function ConnectivityPage() {
 	return (
 		<>
 			<SettingsPage title={"Connectivity"}>
 				<SettingsContainer label="OpenDrop" bottomText="OpenDrop is an open AirDrop implementation. Please note that this feature is experimental and might have to be restarted to work correctly.">
-
+                    <SettingsSwitch label={"Enable AirDrop"} onSwitch={() => {}} />
                 </SettingsContainer>
 			</SettingsPage>
 		</>
