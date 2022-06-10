e350e35fc31c2614423a8d73b5c61f8022244d79
FloatDev <54747366+TheRealFloatDev@users.noreply.github.com>
2022-06-10 12:33:33 +0200

    Added Background blur slider

--- src/components/settings/settingspages/Appearance.tsx
+++ src/components/settings/settingspages/Appearance.tsx
@@ -2,6 +2,7 @@ import { Divider, useMantineColorScheme } from "@mantine/core";
 import * as React from "react";
 import SettingsContainer from "../SettingsContainer";
 import SettingsPage from "../SettingsPage";
+import SettingsSlider from "../SettingsSlider";
 import SettingsSwitch from "../SettingsSwitch";
 
 export default function AppearanceSettings() {
@@ -23,6 +24,9 @@ export default function AppearanceSettings() {
 					onSwitch={() => {}}
 				/>
 				<Divider />
+				<SettingsSlider label={"Background Blur"} min={0} max={100} value={50} onChange={() => {
+
+				}} />
 			</SettingsContainer>
 		</SettingsPage>
 	);
