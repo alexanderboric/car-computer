72cabdbc3a6c69a14e0cf8178f3cc410d9106664
FloatDev <54747366+TheRealFloatDev@users.noreply.github.com>
2022-06-11 19:01:17 +0200

    CreatedSettingsMultiSelect component

---
+++ src/components/settings/SettingsMultiSelect.tsx
@@ -0,0 +1,63 @@
+import {
+	Group,
+	UnstyledButton,
+	Text,
+	useMantineTheme,
+	Title,
+	SegmentedControl,
+} from "@mantine/core";
+import { useModals } from "@mantine/modals";
+import * as React from "react";
+import { useState } from "react";
+import { MdChevronRight } from "react-icons/md";
+
+export default function SettingsMultiSelect({
+	label,
+	value,
+    values,
+	onChange,
+}: {
+	label: string;
+	value: string;
+    values: { value: string, label: string | React.ReactNode }[];
+	onChange: (text: string) => void;
+}) {
+	const theme = useMantineTheme();
+	const modals = useModals();
+	const [selectedLabel, setSelectedLabel] = useState(values.find(v => v.value === value).label);
+
+	return (
+		<>
+			<UnstyledButton
+				pl="xs"
+				pr="xs"
+				style={{ borderRadius: theme.radius.md }}
+				onClick={() => {
+					const id = modals.openModal({
+						title: <Title>{label}</Title>,
+						size: "xl",
+						centered: false,
+						children: (
+							<SegmentedControl orientation="vertical" size="xl" value={value} data={values} onChange={(val: string) => {
+                                setSelectedLabel(values.find(v => v.value === val).label);
+                                onChange(val);
+                            }}/>
+						),
+					});
+				}}
+			>
+				<Group position="apart" noWrap mb={3} mt={3}>
+					<Group>
+						<Text size="xl">{label}</Text>
+					</Group>
+					<Group spacing={5}>
+						<Text weight={550} size="xl">
+							{selectedLabel}
+						</Text>
+						<MdChevronRight size={20} />
+					</Group>
+				</Group>
+			</UnstyledButton>
+		</>
+	);
+}
