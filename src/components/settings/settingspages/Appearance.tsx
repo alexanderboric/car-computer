0d2faa6db5667d6e490f3bb94cd6da4ab68d5c51
FloatDev <54747366+TheRealFloatDev@users.noreply.github.com>
2022-06-11 19:05:33 +0200

    Added OSK Button Radius select

--- src/components/settings/settingspages/Appearance.tsx
+++ src/components/settings/settingspages/Appearance.tsx
@@ -3,6 +3,7 @@ import * as React from "react";
 import { BackgroundContext } from "../../../lib/context";
 import SettingsContainer from "../SettingsContainer";
 import SettingsPage from "../SettingsPage";
+import SettingsSelect from "../SettingsSelect";
 import SettingsSlider from "../SettingsSlider";
 import SettingsSwitch from "../SettingsSwitch";
 
@@ -18,6 +19,7 @@ export default function AppearanceSettings() {
 					onSwitch={() => toggleColorScheme()}
 				/>
 			</SettingsContainer>
+
 			<SettingsContainer label="Background">
 				<SettingsSwitch
 					label={"Use Background Image"}
@@ -32,6 +34,16 @@ export default function AppearanceSettings() {
 				<SettingsSlider label={"Background Blur"} min={0} max={100} value={blur} onChange={(num) => {
 					setBlur(num);
 				}} />
+			</SettingsContainer>
+
+			<SettingsContainer label="On Screen Keyboard" bottomText="On Screen Keyboard will appear whenever text input is required.">
+				<SettingsSelect label={"Radius"} value={"md"} values={[
+					{ label: "Extra Small", value: "xs" },
+					{ label: "Small", value: "sm" },
+					{ label: "Medium", value: "md" },
+					{ label: "Large", value: "lg" },
+					{ label: "Extra Large", value: "xl" }
+				]} onChange={() => {}} />
 			</SettingsContainer>
 		</SettingsPage>
 	);
