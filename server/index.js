bcbbb5a3a491cc34ae5690d841802c62b578b8c0
FloatDev <54747366+TheRealFloatDev@users.noreply.github.com>
2022-06-12 13:53:21 +0200

    Added settings (not working yet)

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const pino = require('express-pino-logger')();
+const fs = require('fs');
 
 const app = express();
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -12,6 +13,47 @@ app.get('/api/greeting', (req, res) => {
   res.send(JSON.stringify({ greeting: `Hello ${name}!` }));
 });
 
+app.get('api/settings/get', (req, res) => {
+  const setting = req.query.setting;
+  res.setHeader('Content-Type', 'application/json');
+  if (!setting) {
+    res.send(JSON.stringify({ error: 'No setting provided' }));
+  } else {
+    let settings = readSettings();
+    if (settings.includes(setting)) {
+      res.send(JSON.stringify({ setting: setting, value: settings[setting] }));
+    } else {
+      res.send(JSON.stringify({ error: 'Setting not found' }));
+    }
+  }
+});
+
+app.get('api/settings/set', (req, res) => {
+  const setting = req.query.setting;
+  const value = req.query.value;
+  res.setHeader('Content-Type', 'application/json');
+  if (!setting) {
+    res.send(JSON.stringify({ error: 'No setting provided' }));
+  } else if (!value) {
+    res.send(JSON.stringify({ error: 'No value provided' }));
+  } else {
+    let settings = readSettings();
+    settings[setting] = value;
+    writeSettings(settings);
+    res.send(JSON.stringify({ setting: setting, value: value }));
+  }
+});
+
 app.listen(3001, () =>
   console.log('Express server is running on localhost:3001')
 );
+
+
+
+function readSettings() {
+  return JSON.parse(fs.readFileSync('./settings.json'));
+}
+
+function writeSettings(settings) {
+  fs.writeFileSync('./settings.json', JSON.stringify({settings}));
+}
--- src/components/Manager.js
+++ src/components/Manager.js
@@ -1,5 +1,5 @@
 import '../App.css';
-import React from 'react';
+import React, { useEffect } from 'react';
 import { useState } from 'react';
 
 import Navbar from './Navbar';
@@ -13,6 +13,27 @@ export default function Manager() {
   const [backgroundImageHomeScreenOnly, setBackgroundImageHomeScreenOnly] = useState(true);
   const [backgroundImageBlur, setBackgroundImageBlur] = useState(35);
 
+  useEffect(() => {
+    fetch('/api/settings/get?setting=useBackgroundImage')
+      .then(res => res.json())
+      .then(data => {
+        setUseBackgroundImage(data.value);
+      });
+    fetch('/api/settings/get?setting=backgroundImageHomeScreenOnly')
+      .then(res => res.json())
+      .then(data => {
+        setBackgroundImageHomeScreenOnly(data.value);
+      });
+  }, []);
+
+  useEffect(() => {
+    fetch('/api/settings/set?setting=useBackgroundImage&value=' + useBackgroundImage);
+  }, [useBackgroundImage]);
+
+  useEffect(() => {
+    fetch('/api/settings/set?setting=backgroundImageBlur&value=' + backgroundImageBlur);
+  }, [backgroundImageBlur]);
+
   return (
     <>
       <div className="App" style={{ display: 'flex', userSelect: "none", WebkitUserSelect: "none" }}  >
@@ -24,7 +45,7 @@ export default function Manager() {
                     
                 </div> */}
         <div style={{ height: "100%" }}>
-          <BackgroundContext.Provider value={{ useImage:useBackgroundImage, setUseImage:setUseBackgroundImage, blur: backgroundImageBlur, setBlur: setBackgroundImageBlur, homeScreenOnly: backgroundImageHomeScreenOnly, setHomeScreenOnly: setBackgroundImageHomeScreenOnly }}  >
+          <BackgroundContext.Provider value={{ useImage: useBackgroundImage, setUseImage: setUseBackgroundImage, blur: backgroundImageBlur, setBlur: setBackgroundImageBlur, homeScreenOnly: backgroundImageHomeScreenOnly, setHomeScreenOnly: setBackgroundImageHomeScreenOnly }}  >
             {useBackgroundImage && !backgroundImageHomeScreenOnly && <BackgroundImage style={{ height: "100%", position: "absolute", filter: `blur(${(15 * (backgroundImageBlur / 100))}px)` }} src="/background.png" />}
             <div style={{ justifyContent: 'center', position: 'absolute', right: 0, top: 0, width: "91%", height: "100%", zIndex: 2 }}>
               <Outlet />
