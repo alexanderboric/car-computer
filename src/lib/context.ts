552d8cdaf2b1c70494538fe1288bb3ac07d6e0e4
FloatDev <54747366+TheRealFloatDev@users.noreply.github.com>
2022-06-16 23:02:11 +0200

    SettingsContext cleanup

--- src/lib/context.ts
+++ src/lib/context.ts
@@ -1,3 +1,28 @@
 import { createContext } from "react";
 
-export const SettingsContext = createContext({useBackgroundImage: false, setUseBackgroundImage: (useImage: boolean) => {}, backgroundImageBlur: 50, setBackgroundImageBlur: (blur: number) => {}, backgroundImageHomeScreenOnly: true, setBackgroundImageHomeScreenOnly: (homeScreenOnly: boolean) => {}, buttonSize: 4, setButtonSize: (size: number) => {}, buttonRadius: 1, setButtonRadius: (radius: number) => {}, fontFamily: null, setFontFamily: (font: string) => {}, settingsLoaded: false});
+export const SettingsContext = createContext({
+    /* -- Background Image -- */
+    useBackgroundImage: false,
+    setUseBackgroundImage: (useImage: boolean) => { },
+    backgroundImageBlur: 50,
+    setBackgroundImageBlur: (blur: number) => { },
+    backgroundImageHomeScreenOnly: true,
+    setBackgroundImageHomeScreenOnly: (homeScreenOnly: boolean) => { },
+    /* -- Background Image -- */
+
+    /* -- On Screen Keyboard -- */
+    buttonSize: 4,
+    setButtonSize: (size: number) => { },
+    buttonRadius: 1,
+    setButtonRadius: (radius: number) => { },
+    /* -- On Screen Keyboard -- */
+
+    /* -- Font -- */
+    fontFamily: null,
+    setFontFamily: (font: string) => { },
+    /* -- Font -- */
+
+    /* -- Misc -- */
+    settingsLoaded: false
+    /* -- Misc -- */
+});
