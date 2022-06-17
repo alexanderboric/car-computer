475538e4a102b8a2eeedc399f671f9274acf7a3d
FloatDev <54747366+TheRealFloatDev@users.noreply.github.com>
2022-06-17 07:59:42 +0200

    Suppressed stupid warning

--- src/components/settings/settingspages/Connectivity.tsx
+++ src/components/settings/settingspages/Connectivity.tsx
@@ -15,6 +15,7 @@ export default function ConnectivityPage() {
 
 	React.useEffect(() => {
 		openDropStatusInterval.start();
+	// eslint-disable-next-line react-hooks/exhaustive-deps
 	}, []);
 
 	const openDropStatusInterval = useInterval(() => {
