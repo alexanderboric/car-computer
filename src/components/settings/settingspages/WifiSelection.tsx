432793bf4d9518a0dd6e56cb1de8a1e0b418efa7
FloatDev <54747366+TheRealFloatDev@users.noreply.github.com>
2022-06-18 19:33:32 +0200

    Added bottom text to my networks in wifi selection

--- src/components/settings/settingspages/WifiSelection.tsx
+++ src/components/settings/settingspages/WifiSelection.tsx
@@ -6,7 +6,7 @@ export default function WifiSelectionPage() {
 	return (
 		<>
 			<SettingsPage title={"WLAN Selection"} backLink="/settings/connectivity">
-                <SettingsContainer label="My Networks">
+                <SettingsContainer label="My Networks" bottomText="Here you can find WLAN networks, which credentials are already saved in the system.">
                 </SettingsContainer>
                 <SettingsContainer label="Available Networks">
                 </SettingsContainer>
