94fa00f6bb93f5d66f39a63122f560b0aa3baf8e
FloatDev <54747366+TheRealFloatDev@users.noreply.github.com>
2022-09-06 14:00:17 +0200

    Started building appstore

--- apps/builtin-appstore/appStore.tsx
+++ apps/builtin-appstore/appStore.tsx
@@ -1,5 +1,30 @@
+import { Group, SegmentedControl, Stack, Text } from "@mantine/core";
 import * as React from "react";
+import { MdDownloadDone, MdSearch, MdSource, MdStar } from "react-icons/md";
 
 export default function AppStoreApp() {
-    return (<></>)
+    return (<>
+    <Stack>
+        <SegmentedControl 
+        size="xl"
+        data={[
+            {
+                value: 'featured',
+                label: <Group><MdStar /><Text weight={600}>Featured</Text></Group>
+            },
+            {
+                value: 'search',
+                label: <Group><MdSearch /><Text weight={600}>Search</Text></Group>
+            },
+            {
+                value: 'installed',
+                label: <Group><MdDownloadDone /><Text weight={600}>Installed</Text></Group>
+            },
+            {
+                value: 'sources',
+                label: <Group><MdSource /><Text weight={600}>Sources</Text></Group>
+            }
+        ]} />
+    </Stack>
+    </>)
 }
